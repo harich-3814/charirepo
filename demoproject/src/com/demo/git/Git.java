1f1e31bb8e919e883b053a5a1fd5c7d511cf0e3d
harich-3814 <hari.ch3814@gmail.com>
2019-12-06 15:57:54 +0530

    adding metjod

--- demoproject/src/com/demo/git/Git.java
+++ demoproject/src/com/demo/git/Git.java
@@ -1,11 +1,17 @@
 package com.demo.git;
 
 public class Git {
+	static int get(int a,int b)
+	{
+		return a+b;
+	}
 	public static void main(String args[])
 	{
 		System.out.println("first print after project added");
-		int a;
-		int c;
+		int d;
+		d=get(10,88);
+		System.out.println(d);
+		
 	}
 
 }
